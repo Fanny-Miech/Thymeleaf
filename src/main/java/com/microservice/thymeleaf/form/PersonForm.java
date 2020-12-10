cc78c7d5d5969fc39cf557065ede60477b3c2e34
fanny <fanny.miech@le-campus-numerique.fr>
2020-12-10 09:12:18 +0100

    implement templates to display a list of person and a form to add a person

---
+++ src/main/java/com/microservice/thymeleaf/form/PersonForm.java
@@ -0,0 +1,15 @@
+package com.microservice.thymeleaf.form;
+
+public class PersonForm {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
